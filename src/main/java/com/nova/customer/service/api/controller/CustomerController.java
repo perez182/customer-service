5744975592d4d541d958d6cf4f3237378c8c6cd5
unknown <pbautistadavid@gmail.com>
2026-04-29 17:09:43 -0600

    feat: add controller and controlleradvice

---
+++ src/main/java/com/nova/customer/service/api/controller/CustomerController.java
@@ -0,0 +1,68 @@
+package com.nova.customer.service.api.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nova.customer.service.api.dto.request.CustomerCreateReq;
+import com.nova.customer.service.api.dto.request.CustomerUpdateReq;
+import com.nova.customer.service.api.dto.response.CustomerRes;
+import com.nova.customer.service.service.CustomerService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("customers")
+@Validated
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<CustomerRes> create(@Valid @RequestBody CustomerCreateReq req) {
+        CustomerRes response = customerService.create(req);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerRes>> getAll() {
+        List<CustomerRes> customers = customerService.getAll();
+
+        return ResponseEntity.ok(customers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerRes> getById(@PathVariable Long id) {
+        CustomerRes customer = customerService.getCustomerByID(id);
+
+        return ResponseEntity.ok(customer);
+    }
+
+    @PutMapping
+    public ResponseEntity<CustomerRes> update(@Valid @RequestBody CustomerUpdateReq req) {
+        CustomerRes response = customerService.update(req);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        customerService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/nova/customer/service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,59 @@
+package com.nova.customer.service.exception;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidation(MethodArgumentNotValidException ex) {
+        List<Map<String, String>> details = ex.getBindingResult()
+            .getFieldErrors()
+                .stream()
+                .map(error -> {
+                    Map<String, String> map = new HashMap<>();
+                    map.put("field", error.getField()); 
+                    map.put("error", error.getDefaultMessage());
+                    return map;
+                }).toList();
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("errors", details);
+        
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, List<String>>> handleReadableException(HttpMessageNotReadableException ex) {
+        Map<String, List<String>> response = new HashMap<>();
+        String error="Invalid format in one or more fields (check the fields birhtdate,age,isActive)";
+        List<String> errors = new ArrayList<>();
+        errors.add(error);
+        response.put("errors", errors);
+        
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map<String, List<String>>>  handleNotFound(EntityNotFoundException ex) {
+        List<String> errors = new ArrayList<>();
+        errors.add(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
+            "errors", errors
+        ));
+    }
+
+
+}
